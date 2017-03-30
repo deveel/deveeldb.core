da4c3de71db9b1dd421861a44202a47fd2ed844e
Antonello Provenzano <antonello@deveel.com>
2017-03-30 16:30:24 +0200

    Travis packing destinations

--- travis-pack.sh
+++ travis-pack.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 num=$TRAVIS_BUILD_NUMBER
 VERSION=$(printf "v%05d\n" $num)
+PACKAGES=./packages/$TRAVIS_BUILD_NUMBER
 
 echo "version suffix to $VERSION"
 
-if [ ! -d ./packages/$TRAVIS_BUILD_NUMBER ]; then
-  mkdir -p ./packages/$TRAVIS_BUILD_NUMBER;
+if [ ! -d $PACKAGES ]; then
+  mkdir -p $PACKAGES;
 fi
 
 for f in ./test/**/*.csproj;
 do
-	dotnet pack $f -c release --version-suffix $VERSION -o ./packages/$TRAVIS_BUILD_NUMBER
+	dotnet pack $f -c release --version-suffix $VERSION -o $PACKAGES
 done
