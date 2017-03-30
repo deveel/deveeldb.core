21a0e4eed28225f5509df99051088d28f6049436
Antonello Provenzano <antonello@deveel.com>
2017-03-30 18:29:24 +0200

    AppVeyor test scripts

--- scripts/appveyor-tests.bat
+++ scripts/appveyor-tests.bat
@@ -1,3 +1,6 @@
-for %%F in (%APPVEYOR_BUILD_FOLDER%\test\**\*.csproj) do (
-	dotnet test %%F -c Release
+echo "scanning tests in %APPVEYOR_BUILD_FOLDER%\test\"
+
+for %%f in (%APPVEYOR_BUILD_FOLDER%\test\**\*.csproj) do (
+	echo "testing %%~nf"
+	dotnet test %%~nf -c Release
 )
