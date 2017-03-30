ae11db2365f8d3ae6c89e702063fb5268b858602
Antonello Provenzano <antonello@deveel.com>
2017-03-30 15:39:56 +0200

    version in packing

--- travis-pack.sh
+++ travis-pack.sh
@@ -3,7 +3,9 @@ if [ ! -d ./packages/$TRAVIS_BUILD_NUMBER ]; then
   mkdir -p ./packages/$TRAVIS_BUILD_NUMBER;
 fi
 
+VERSION = "v%50g" $$TRAVIS_BUILD_NUMBER
+
 for f in ./test/**/*.csproj;
 do
-	dotnet pack $f -c release --version-suffix $TRAVIS_BUILD_NUMBER -o ./packages/$TRAVIS_BUILD_NUMBER
+	dotnet pack $f -c release --version-suffix $VERSION -o ./packages/$TRAVIS_BUILD_NUMBER
 done
