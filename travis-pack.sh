743ef2c909d8bcb3aa8ef023d22681da9bcc3ebe
Antonello Provenzano <antonello@deveel.com>
2017-03-30 12:24:58 +0200

    some tweaks in the Travis pack script

--- travis-pack.sh
+++ travis-pack.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+if [ ! -d ./packages/$TRAVIS_BUILD_NUMBER ]; then
+  mkdir -p ./packages/$TRAVIS_BUILD_NUMBER;
+fi
+
 FILES = ./test/**/*.csproj
 for f in $FILES
 do
-	dotnet pack f --version-suffix $TRAVIS_BUILD_NUMBER
+	dotnet pack f -c release --version-suffix $TRAVIS_BUILD_NUMBER -o ./packages/$TRAVIS_BUILD_NUMBER
 done
