25f31da7a7f36487f53eaaf2ce22aaabf67a1c37
Antonello Provenzano <antonello@deveel.com>
2017-04-12 09:22:04 +0200

    Mutable table skeleton

---
+++ src/DeveelDb.Core/Data/Sql/Tables/IMutableTable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Deveel.Data.Sql.Tables {
+	public interface IMutableTable : ITable {
+		RowId AddRow(Row row);
+
+		void UpdateRow(Row row);
+
+		void RemoveRow(RowId rowId);
+	}
+}
