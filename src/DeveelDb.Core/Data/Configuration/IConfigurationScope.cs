1341c02c9dd35d2bf7acad1c688094f4f83c5bdb
Antonello Provenzano <antonello@deveel.com>
2017-04-16 01:09:23 +0200

    Configuration context and license headers

--- src/DeveelDb.Core/Data/Configuration/IConfigurationScope.cs
+++ src/DeveelDb.Core/Data/Configuration/IConfigurationScope.cs
@@ -17,24 +17,8 @@
 
 using System;
 
-using Deveel.Data.Sql.Expressions;
-
-namespace Deveel.Data.Sql.Methods {
-	public sealed class MergeContext : Context {
-		internal MergeContext(MethodContext parent, SqlObject accumulated)
-			: base(parent, $"Aggregate({parent.Method.MethodInfo.MethodName})") {
-			Accumulated = accumulated;
-		}
-
-		public SqlObject Accumulated { get; }
-
-		internal SqlObject Output { get; private set; }
-
-		public void SetOutput(SqlObject output) {
-			if (output.IsNull)
-				throw new NotSupportedException();
-
-			Output = output;
-		}
+namespace Deveel.Data.Configuration {
+	public interface IConfigurationScope {
+		IConfiguration Configuration { get; }
 	}
 }
