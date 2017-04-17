b7cfb2aab4e74694feba1866cfb682fdb01267fe
Antonello Provenzano <antonello@deveel.com>
2017-04-17 09:48:22 +0200

    Context extensions for object queries support

--- src/DeveelDb.Core/Data/ContextExtensions.cs
+++ src/DeveelDb.Core/Data/ContextExtensions.cs
@@ -17,7 +17,10 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
+using Deveel.Data.Configuration;
 using Deveel.Data.Services;
 
 namespace Deveel.Data {
@@ -46,6 +49,85 @@ namespace Deveel.Data {
 
 		public static IEnumerable<TService> ResolveAllServices<TService>(this IContext context) {
 			return context.Scope.ResolveAll<TService>();
+		}
+
+		public static IEnumerable<IDbObjectManager> GetObjectManagers(this IContext context, DbObjectType objectType) {
+			return context.ResolveAllServices<IDbObjectManager>()
+				.Where(x => x.ObjectType == objectType);
+		}
+
+		public static IEnumerable<IDbObjectManager> GetObjectManagers(this IContext context) {
+			return context.ResolveAllServices<IDbObjectManager>();
+
+		}
+
+		public static async Task<IDbObject> GetObjectAsync(this IContext context, DbObjectType objectType,
+			ObjectName objectName) {
+			var managers = context.GetObjectManagers(objectType);
+			foreach (var manager in managers) {
+				var obj = await manager.GetObjectAsync(objectName);
+				if (obj != null)
+					return obj;
+			}
+
+			return null;
+		}
+
+		public static async Task<DbObjectType> GetObjectType(this IContext context, ObjectName objectName) {
+			var managers = context.GetObjectManagers();
+			foreach (var manager in managers) {
+				if (await manager.ObjectExistsAsync(objectName))
+					return manager.ObjectType;
+			}
+
+			throw new InvalidOperationException($"Object {objectName} not found in this context");
+		}
+
+		public static async Task<bool> ObjectExistsAsync(this IContext context, DbObjectType objectType, ObjectName objectName) {
+			var managers = context.GetObjectManagers(objectType);
+			foreach (var manager in managers) {
+				if (await manager.ObjectExistsAsync(objectName))
+					return true;
+			}
+
+			return false;
+		}
+
+		public static async Task<IDbObjectInfo> GetObjectInfo(this IContext context, DbObjectType objectType, ObjectName objectName) {
+			var managers = context.GetObjectManagers(objectType);
+			foreach (var manager in managers) {
+				var objInfo = await manager.GetObjectInfoAsync(objectName);
+				if (objInfo != null)
+					return objInfo;
+			}
+
+			return null;
+		}
+
+		public static ObjectName QualifyName(this IContext context, ObjectName objectName) {
+			if (objectName.Parent == null) {
+				var currentSchema = context.GetValue<string>("currentSchema", null);
+				if (String.IsNullOrWhiteSpace(currentSchema))
+					throw new InvalidOperationException("None schema was set in context");
+
+				objectName = new ObjectName(new ObjectName(currentSchema), objectName.Name);
+			}
+
+			return objectName;
+		}
+
+		public static Task<ObjectName> ResolveNameAsync(this IContext context, ObjectName objectName) {
+			var ignoreCase = context.GetValue("ignoreCase", true);
+
+			var name = context.QualifyName(objectName);
+			var managers = context.GetObjectManagers();
+			foreach (var manager in managers) {
+				var resolved = manager.ResolveNameAsync(name, ignoreCase);
+				if (resolved != null)
+					return resolved;
+			}
+
+			return null;
 		}
 	}
 }
