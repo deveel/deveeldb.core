76971d8bb97693e0ca90ffc4f2df2494b56da689
Antonello Provenzano <antonello@deveel.com>
2017-04-22 01:30:47 +0200

    In-Memory store

---
+++ src/DeveelDb.Core/Data/Storage/IStoreSystem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Deveel.Data.Configuration;
+
+namespace Deveel.Data.Storage {
+	public interface IStoreSystem : IDisposable {
+		Task<bool> StoreExistsAsync(IConfiguration configuration);
+
+		Task<IStore> CreateStoreAsync(IConfiguration configuration);
+
+		Task<IStore> OpenStoreAsync(IConfiguration configuration);
+
+		Task<bool> CloseStoreAsync(IStore store);
+
+		Task<bool> DeleteStoreAsync(IStore store);
+
+		Task SetCheckPointAsync();
+
+		Task LockAsync(string lockKey);
+
+		Task UnlockAsync(string lockKey);
+	}
+}
