7bc92db4f7186e951eee3f32d3c8d78cb861693b
Antonello Provenzano <antonello@deveel.com>
2017-04-28 21:02:23 +0200

    serialization util

---
+++ test/DeveelDb.Core.Tests/Data/Serialization/SerializationUtil.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace Deveel.Data.Serialization {
+	static class SerializationUtil {
+		public static T BinarySerialize<T>(T obj)
+			where T : class {
+			var serializer = new BinarySerializer();
+			var stream = new MemoryStream();
+			serializer.Serialize(obj, stream);
+
+			stream.Seek(0, SeekOrigin.Begin);
+
+			return (T) serializer.Deserialize(typeof(T), stream);
+		}
+	}
+}
