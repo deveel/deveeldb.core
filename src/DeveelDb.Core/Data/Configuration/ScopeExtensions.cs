aea628070439d905935b2afed9eacbd4d5931bd0
Antonello Provenzano <antonello@deveel.com>
2017-04-29 18:48:11 +0200

    Configurations in scope

---
+++ src/DeveelDb.Core/Data/Configuration/ScopeExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using Deveel.Data.Services;
+
+namespace Deveel.Data.Configuration {
+	public static class ScopeExtensions {
+		public static void SetConfiguration(this IScope scope, IConfiguration configuration) {
+			var config = scope.Resolve<IConfiguration>();
+			var final = configuration;
+			if (config != null)
+				final = final.MergeWith(configuration);
+
+			scope.Unregister<IConfiguration>();
+			scope.RegisterInstance<IConfiguration>(final);
+		}
+	}
+}
