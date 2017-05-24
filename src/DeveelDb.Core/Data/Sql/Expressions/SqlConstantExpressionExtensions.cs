5974c8a9c4f225215b0828fcbd80c4555d63eab6
Antonello Provenzano <antonello@deveel.com>
2017-05-24 12:19:54 +0200

    Fixes after merge

--- src/DeveelDb.Core/Data/Sql/Expressions/SqlConstantExpressionExtensions.cs
+++ src/DeveelDb.Core/Data/Sql/Expressions/SqlConstantExpressionExtensions.cs
@@ -3,14 +3,6 @@ using System.Threading.Tasks;
 
 namespace Deveel.Data.Sql.Expressions {
 	static class SqlConstantExpressionExtensions {
-		public static async Task<SqlObject> ReduceToConstantAsync(this SqlExpression expression, IContext context) {
-			var reduced = await expression.ReduceAsync(context);
-			if (reduced.ExpressionType != SqlExpressionType.Constant)
-				throw new SqlExpressionException("The expression was not reduced to constant");
-
-			return ((SqlConstantExpression) reduced).Value;
-		} 
-
 		public static bool IsConstant(this SqlExpression expression) {
 			var visitor = new SqlConstantExpressionVisitor();
 			visitor.Visit(expression);
