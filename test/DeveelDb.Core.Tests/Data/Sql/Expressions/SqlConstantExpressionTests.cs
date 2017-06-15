b92db75c77f064503f90c46c1101d2c6eb1a6e4e
Antonello Provenzano <antonello@deveel.com>
2017-06-15 08:42:58 +0200

    Splitting expression tests in separate files

---
+++ test/DeveelDb.Core.Tests/Data/Sql/Expressions/SqlConstantExpressionTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Deveel.Data.Serialization;
+
+using Xunit;
+
+namespace Deveel.Data.Sql.Expressions {
+	public class SqlConstantExpressionTests {
+		[Theory]
+		[InlineData(65775.499)]
+		[InlineData("The quick brown fox")]
+		public static void CreateConstant(object value) {
+			var obj = SqlObject.New(SqlValueUtil.FromObject(value));
+			var exp = SqlExpression.Constant(obj);
+
+			Assert.NotNull(exp.Value);
+			Assert.Equal(obj, exp.Value);
+		}
+
+		[Theory]
+		[InlineData(65775.499)]
+		[InlineData("The quick brown fox")]
+		public static void SerializeConstant(object value) {
+			var obj = SqlObject.New(SqlValueUtil.FromObject(value));
+			var exp = SqlExpression.Constant(obj);
+
+			var result = BinarySerializeUtil.Serialize(exp);
+
+			Assert.Equal(exp.ExpressionType, result.ExpressionType);
+			Assert.Equal(exp.Value, result.Value);
+		}
+
+		[Theory]
+		[InlineData(345)]
+		public static async void ReduceConstant(object value) {
+			var obj = SqlObject.New(SqlValueUtil.FromObject(value));
+			var exp = SqlExpression.Constant(obj);
+
+			Assert.False(exp.CanReduce);
+			Assert.NotNull(exp.Value);
+
+			var reduced = await exp.ReduceAsync(null);
+
+			Assert.IsType<SqlConstantExpression>(reduced);
+			Assert.Equal(obj, ((SqlConstantExpression)reduced).Value);
+		}
+	}
+}
---
+++ test/DeveelDb.Core.Tests/Data/Sql/Expressions/SqlUnaryExpressionTests.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Deveel.Data.Serialization;
+
+using Xunit;
+
+namespace Deveel.Data.Sql.Expressions {
+	public static class SqlUnaryExpressionTests {
+		[Theory]
+		[InlineData(SqlExpressionType.UnaryPlus, 22.34)]
+		[InlineData(SqlExpressionType.Negate, 455.43)]
+		[InlineData(SqlExpressionType.Not, true)]
+		public static void CreateUnary(SqlExpressionType expressionType, object value) {
+			var obj = SqlObject.New(SqlValueUtil.FromObject(value));
+			var operand = SqlExpression.Constant(obj);
+			var exp = SqlExpression.Unary(expressionType, operand);
+
+			Assert.NotNull(exp.Operand);
+			Assert.IsType<SqlConstantExpression>(exp.Operand);
+		}
+
+		[Theory]
+		[InlineData(SqlExpressionType.UnaryPlus, 22.34)]
+		[InlineData(SqlExpressionType.Negate, 455.43)]
+		[InlineData(SqlExpressionType.Not, true)]
+		public static void SerializeUnary(SqlExpressionType expressionType, object value) {
+			var obj = SqlObject.New(SqlValueUtil.FromObject(value));
+			var operand = SqlExpression.Constant(obj);
+			var exp = SqlExpression.Unary(expressionType, operand);
+
+			var result = BinarySerializeUtil.Serialize(exp);
+
+			Assert.Equal(exp.ExpressionType, result.ExpressionType);
+		}
+
+		[Theory]
+		[InlineData(SqlExpressionType.UnaryPlus, 22.34, 22.34)]
+		[InlineData(SqlExpressionType.Negate, 455.43, -455.43)]
+		[InlineData(SqlExpressionType.Not, true, false)]
+		public static async Task ReduceUnary(SqlExpressionType expressionType, object value, object expected) {
+			var obj = SqlObject.New(SqlValueUtil.FromObject(value));
+			var operand = SqlExpression.Constant(obj);
+			var exp = SqlExpression.Unary(expressionType, operand);
+
+			Assert.NotNull(exp.Operand);
+			Assert.IsType<SqlConstantExpression>(exp.Operand);
+
+			Assert.True(exp.CanReduce);
+
+			var reduced = await exp.ReduceAsync(null);
+
+			Assert.NotNull(reduced);
+			Assert.IsType<SqlConstantExpression>(reduced);
+
+			var expectedResult = SqlObject.New(SqlValueUtil.FromObject(expected));
+			Assert.Equal(expectedResult, ((SqlConstantExpression)reduced).Value);
+		}
+
+		[Theory]
+		[InlineData(SqlExpressionType.UnaryPlus, 22.34, "+22.34")]
+		[InlineData(SqlExpressionType.Negate, 455.43, "-455.43")]
+		[InlineData(SqlExpressionType.Not, true, "~TRUE")]
+		public static void GetUnaryString(SqlExpressionType expressionType, object value, string expected) {
+			var obj = SqlObject.New(SqlValueUtil.FromObject(value));
+			var operand = SqlExpression.Constant(obj);
+			var exp = SqlExpression.Unary(expressionType, operand);
+
+			var s = exp.ToString();
+			Assert.Equal(expected, s);
+		}
+
+		[Theory]
+		[InlineData(SqlExpressionType.UnaryPlus, 22.34)]
+		[InlineData(SqlExpressionType.Negate, 455.43)]
+		[InlineData(SqlExpressionType.Not, true)]
+		public static void GetUnaryType(SqlExpressionType expressionType, object value) {
+			var obj = SqlObject.New(SqlValueUtil.FromObject(value));
+			var operand = SqlExpression.Constant(obj);
+			var exp = SqlExpression.Unary(expressionType, operand);
+
+			var sqlType = exp.Type;
+			Assert.Equal(obj.Type, sqlType);
+		}
+	}
+}
